d26146752abfb6979c5cc1d64a97a80ba1471bbf
Maria Rita Boareto <mariaritaboareto@gmail.com>
2026-01-06 19:37:54 -0300

    feat: update CORS configuration to support multiple methods and headers

--- backend/index.ts
+++ backend/index.ts
@@ -9,15 +9,25 @@ async function main() {
 
   const corsOptions = {
     origin: "http://localhost:5173/",
-    methods: "GET",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     preflightContinue: false,
     optionSuccessStatus: 204,
-    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
   };
 
   app.use(express.json());
   app.use(appRoutes);
   app.use(cors(corsOptions));
+  app.use((req, res) => {
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, PUT, PATCH, POST, DELETE, OPTIONS"
+    );
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+  });
 
   app.listen(port, () => {
     console.log(`a porta ${port} ta abrida !`);
