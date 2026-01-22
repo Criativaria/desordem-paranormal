52aa0ef6a13d7e9cd8d9e2e4bb739e23f6c0f710
Maria Rita Boareto <mariaritaboareto@gmail.com>
2026-01-22 20:09:22 -0300

    fix: cronjob not handling async functions

--- backend/src/utils/cron-job.ts
+++ backend/src/utils/cron-job.ts
@@ -1,13 +1,19 @@
 import { CronJob } from "cron";
 import { WikiService } from "../services/wiki-service";
 
-new CronJob(
+export const job = new CronJob(
   "0 0 * * *",
-  function () {
-    WikiService.updatePageConnections();
+  async function () {
+    console.log("Iniciando cronjob de conexões...");
+    try {
+      await WikiService.updatePageConnections();
+      console.log("Cronjob finalizado com sucesso.");
+    } catch (error) {
+      console.error("Erro no cronjob:", error);
+    }
   },
   null,
   true,
   undefined,
-  undefined
+  undefined,
 );
